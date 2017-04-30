c7125b22fb5035ec4c039625022f1704bf276557
riverajefer <jefersonpatino@yahoo.es>
2017-04-29 23:42:32 -0500

    home 29-04

--- js/main.js
+++ js/main.js
@@ -28,6 +28,19 @@ $( document ).ready(function() {
     pause: "false"
     });
 
+  $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:true,
+        navigation:false,
+        navigationText:["",""],
+        slideSpeed:1000,
+        singleItem:true,
+        transitionStyle:"fade",
+        autoPlay:true
+    });
 
 });
 
